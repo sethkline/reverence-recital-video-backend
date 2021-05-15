7722876b7cece44170f4fa735595b3bd0037de27
Seth <seth.mkline@gmail.com>
2021-05-15 16:11:37 -0400

    database config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-module.exports = ({ env }) => ({
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+      options: {}
+    }
+  }
 });

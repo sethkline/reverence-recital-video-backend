c09835aa2e799115ed807a5a66e252b4aa131a05
Seth <seth.mkline@gmail.com>
2021-05-15 22:43:42 -0400

    update database settings

--- config/database.js
+++ config/database.js
@@ -1,23 +1,19 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = () => ({
-  defaultConnection: 'default',
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
         ssl: {
-          rejectUnauthorized: false
-        }
+          ca: env("DATABASE_CA", ""),
+        },
       },
-      options: {}
-    }
-  }
+    },
+  },
 });
